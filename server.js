a11849586987d658e089b2fd1b6672302a452347
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-09 16:53:40 -0700

    moved logger to right file

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+server.use(logger);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const time = new Date().toISOString();
+  console.log(`[${time}] ${req.ip} ${req.method} ${req.url}`);
+
+  next();
+}
 
 module.exports = server;
